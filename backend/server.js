bec42f0c3d18b80a36806f7c6527bb7cc293cf1a
Natalia Pakhomova <trinitynataly@gmail.com>
2025-09-30 00:26:33 +1000

    backend: add test script and minimal test

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ Entry point for starting the Express server.
 const app = require('./app');
 
 // Set the server port to the value from the environment variable or default to 5000
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
